eeaa877d68dc7fc25e9bea644c7b07dd6aa6380a
KangTaeGong <smlee8577@naver.com>
2022-09-19 13:27:59 +0900

    [Add] join, join_validate Test code

--- reviews/src/test/java/project/reviews/service/UserServiceTest.java
+++ reviews/src/test/java/project/reviews/service/UserServiceTest.java
@@ -9,9 +9,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import project.reviews.domain.User;
 import project.reviews.repository.UserRepository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -31,6 +35,22 @@ public class UserServiceTest {
         //when
         Long savedId = userService.join(user);
         //then
-        Assertions.assertEquals(user, userRepository.findById(savedId).get());
+        assertEquals(user, userRepository.findById(savedId).get());
+    }
+
+    /*
+    * 2022-09-19
+    * 회원 가입 ID 중복 확인 테스트
+    * */
+    @Test
+    void join_validate() {
+        //given
+        User user1 = new User("홍기동", "aaa123", "olddogz222@", "olddogz222@");
+        User user2 = new User("이길동", "aaa123", "olddogz111@", "olddogz111@");
+        //when
+        Long savedId1 = userService.join(user1);
+        //then
+        assertThatThrownBy(() -> userService.join(user2)).isInstanceOf(IllegalStateException.class);
+
     }
 }

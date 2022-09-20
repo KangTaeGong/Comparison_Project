7f29c300a6c95e1330fb422a19922d1f9ae9f880
KangTaeGong <smlee8577@naver.com>
2022-09-20 16:27:42 +0900

    [Create] LoginService.java

---
+++ reviews/src/main/java/project/reviews/login/LoginService.java
@@ -0,0 +1,28 @@
+package project.reviews.login;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import project.reviews.domain.User;
+import project.reviews.repository.UserRepository;
+
+/*
+* 2022-09-20
+* Login 관련 Service 로직
+* */
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    
+    private final UserRepository userRepository;
+
+    /*
+    * return값이 null이면 로그인 실패
+    * */
+    public User login(String userId, String password) {
+        return userRepository.findByUserId(userId)
+                .filter(user -> user.getPassword().equals(password))
+                .orElse(null);
+
+    }
+    
+}

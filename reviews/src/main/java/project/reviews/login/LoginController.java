932bf12dc1e7af393b8f75ae021962a0fcc0dd7c
KangTaeGong <smlee8577@naver.com>
2022-09-20 16:27:06 +0900

    [Create] LoginController.java

---
+++ reviews/src/main/java/project/reviews/login/LoginController.java
@@ -0,0 +1,72 @@
+package project.reviews.login;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import project.reviews.domain.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+/*
+* 2022-09-20
+* 로그인 Controller
+* */
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
+        return "login/loginPage";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+                        HttpServletRequest request) {
+
+        if(bindingResult.hasErrors()) {
+            return "login/loginPage";
+        }
+
+        User loginUser = loginService.login(form.getLoginId(), form.getPassword());
+
+        if(loginUser == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 일치하지 않습니다.");
+            return "login/loginPage";
+        }
+
+        /*
+        * 로그인 성공 로직
+        * 세션이 있으면 세션 반환, 없으면 신규 세션 생성
+        * */
+        HttpSession session = request.getSession();
+
+        // 세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_USER, loginUser);
+
+        return "redirect:/";
+    }
+
+    /*
+    * 로그아웃 로직
+    * */
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if(session != null) {
+            session.invalidate();
+        }
+
+        return "redirect:/";
+    }
+}

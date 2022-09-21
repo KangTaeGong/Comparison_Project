357d66b0aa0faad1fbd20b6e38b47d762a42fb9a
KangTaeGong <smlee8577@naver.com>
2022-09-21 19:10:32 +0900

    [Add] @GetMapping("/")... code

--- reviews/src/main/java/project/reviews/controller/HomeController.java
+++ reviews/src/main/java/project/reviews/controller/HomeController.java
@@ -2,8 +2,31 @@ package project.reviews.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import project.reviews.domain.User;
+import project.reviews.login.SessionConst;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false)
+                       User loginUser, Model model) {
+        /*
+        * 세션에 회원 데이터가 없을 시 동작
+        * 로그인이 되지 않은 메인 페이지
+        * */
+        if(loginUser == null) {
+            return "main/mainPage";
+        }
+        
+        /*
+        * 세션이 유지되면 로그인된 메인페이지로 이동
+        * */
+        model.addAttribute("user", loginUser);
+        return "main/loginMainPage";
+    }
 }

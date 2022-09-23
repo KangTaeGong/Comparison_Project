2b4f7db781be6d5a4242fe85c0facf34d5d52463
KangTaeGong <smlee8577@naver.com>
2022-09-23 19:46:25 +0900

    [Modify]  @NoArgsConstructor -> @AllArgsConstructor

--- reviews/src/main/java/project/reviews/dto/FindUserDto.java
+++ reviews/src/main/java/project/reviews/dto/FindUserDto.java
@@ -1,14 +1,15 @@
 package project.reviews.dto;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /*
 * 2022-09-16 생성
 * 패스워드를 제외한 회원의 정보를 가져오기 위한 DTO
 * */
-@Data
-@NoArgsConstructor
+@Getter
+@AllArgsConstructor
 public class FindUserDto {
 
     private String userId;
--- reviews/src/main/java/project/reviews/login/LoginForm.java
+++ reviews/src/main/java/project/reviews/login/LoginForm.java
@@ -1,6 +1,8 @@
 package project.reviews.login;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 import javax.validation.constraints.NotEmpty;
 
@@ -8,12 +10,14 @@ import javax.validation.constraints.NotEmpty;
 * 2022-09-20
 * 로그인시 입력받는 값
 * */
+
 @Data
+@AllArgsConstructor
 public class LoginForm {
 
-    @NotEmpty
+    @NotEmpty(message = "아이디를 입력해주세요.")
     private String loginId;
 
-    @NotEmpty
+    @NotEmpty(message = "비밀번호를 입력해주세요.")
     private String password;
 }

69cbe9eb0b49404afca0258cde5dc95f4f258740
KangTaeGong <smlee8577@naver.com>
2022-09-24 15:13:45 +0900

    [Create] JoinForm.java - User -> JoinForm

---
+++ reviews/src/main/java/project/reviews/login/JoinForm.java
@@ -0,0 +1,43 @@
+package project.reviews.login;
+
+import lombok.Getter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+/*
+ * 2022-09-22
+ * 회원가입 시 회원의 정보를 입력받는 form
+ * */
+@Getter
+public class JoinForm {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotBlank
+    @Pattern(regexp = "^[가-힣a-zA-Z]{2,10}$")
+    private String userName;
+
+    @NotBlank
+    @Pattern(regexp = "[a-z0-9]{5,9}")
+    private String userId;
+
+    @NotBlank
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}")
+    private String password;
+
+    @NotBlank
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}")
+    private String check_password;  // 2차 비밀번호
+
+    public JoinForm(String userName, String userId, String password, String check_password) {
+        this.userName = userName;
+        this.userId = userId;
+        this.password = password;
+        this.check_password = check_password;
+    }
+}

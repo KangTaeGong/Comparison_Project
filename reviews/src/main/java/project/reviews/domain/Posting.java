d025f2183714e76310c450d7f35148651dbc17ca
KangTaeGong <smlee8577@naver.com>
2022-09-27 19:36:42 +0900

    [Add] Posting.java - add field(content)

--- reviews/src/main/java/project/reviews/domain/Posting.java
+++ reviews/src/main/java/project/reviews/domain/Posting.java
@@ -1,11 +1,15 @@
 package project.reviews.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 /*
 * 2022-09-14 생성
@@ -13,14 +17,22 @@ import javax.persistence.Id;
 * */
 @Entity
 @Getter
-public class Posting {
+@NoArgsConstructor
+public class Posting extends BaseTimeEntity {
 
     @Id @GeneratedValue
     @Column(name = "posting_id")
     private Long id;
 
-    private String title;   // 글 제목
-    private String userName; //글쓴이
-    private String hits; // 조회수
+    private String title;   // 제목
+    private String content; // 내용
+    private String writer; //글쓴이
+    private int hits; // 조회수
 
+    public Posting(String title, String content, String writer, int hits) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.hits = hits;
+    }
 }

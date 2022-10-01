2b8719a40f3d86c53415bbe96e37f9e3fa27a803
KangTaeGong <smlee8577@naver.com>
2022-10-01 17:55:57 +0900

    [Add] PostingRepositoryImplTest : delete_posting_Test()

--- reviews/src/test/java/project/reviews/repository/PostingRepositoryImplTest.java
+++ reviews/src/test/java/project/reviews/repository/PostingRepositoryImplTest.java
@@ -64,4 +64,26 @@ public class PostingRepositoryImplTest {
         //then
         Assertions.assertEquals(3, findList.size());
     }
+    
+    /*
+    * 게시글 삭제 테스트
+    * */
+    @Test
+    void delete_posting_Test() {
+        //given
+        Posting posting1 = new Posting("게시글1", "안녕하세요. 홍길동 입니다.", "홍길동", 1);
+        Posting posting2 = new Posting("게시글2", "안녕하세요. 고길동 입니다.", "고길동", 1);
+        Posting posting3 = new Posting("게시글3", "안녕하세요. 김길이 입니다.", "김길이", 1);
+
+        //when
+        postingRepository.create(posting1);
+        postingRepository.create(posting2);
+        postingRepository.create(posting3);
+
+        postingRepository.delete_Posting(posting2.getId());
+        List<PostingResponseDto> findList = postingRepository.getList();
+
+        //then
+        Assertions.assertEquals(2, findList.size());
+    }
 }

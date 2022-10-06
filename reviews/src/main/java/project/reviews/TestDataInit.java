02c040dee421c3d52077bcb89f0d62c8303401e4
KangTaeGong <smlee8577@naver.com>
2022-10-06 18:29:56 +0900

    [Add] TestDataInit.java : @PostConstruct  -> init_posting()

--- reviews/src/main/java/project/reviews/TestDataInit.java
+++ reviews/src/main/java/project/reviews/TestDataInit.java
@@ -2,7 +2,9 @@ package project.reviews;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import project.reviews.dto.PostingForm;
 import project.reviews.login.JoinForm;
+import project.reviews.service.PostingService;
 import project.reviews.service.UserService;
 
 import javax.annotation.PostConstruct;
@@ -16,6 +18,7 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
 
     private final UserService userService;
+    private final PostingService postingService;
 
     /*
     * 테스트용 회원 아이디 저장
@@ -24,5 +27,14 @@ public class TestDataInit {
     public void init() {
         JoinForm form = new JoinForm("홍길동", "aaa123", "aaaa1234@", "aaaa1234@");
         userService.join(form);
+    }
+    
+    /*
+    * 게시글 하나 미리 생성
+    * */
+    @PostConstruct
+    public void init_posting() {
+        PostingForm form = new PostingForm("첫 번째 게시글", "안녕하세요", "홍길동", "aaaa1234@");
+        postingService.create_posting(form);
     }
 }

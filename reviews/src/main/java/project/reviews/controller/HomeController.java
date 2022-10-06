d7c6a60c487dfb7d6daac005c328956ec18f93d1
KangTaeGong <smlee8577@naver.com>
2022-10-06 18:23:25 +0900

    [Modify] HomeController : return loginMainPage -> mainPage

--- reviews/src/main/java/project/reviews/controller/HomeController.java
+++ reviews/src/main/java/project/reviews/controller/HomeController.java
@@ -20,8 +20,10 @@ public class HomeController {
         /*
         * 세션에 회원 데이터가 없을 시 동작
         * 로그인이 되지 않은 메인 페이지
+        * thymeleaf에 null값도 같이 넘겨주어 로그인 여부 확인
         * */
         if(loginUser == null) {
+            model.addAttribute("user", loginUser);
             return "main/mainPage";
         }
         
@@ -30,6 +32,6 @@ public class HomeController {
         * 세션이 유지되면 로그인된 메인페이지로 이동
         * */
         model.addAttribute("user", loginUser);
-        return "main/loginMainPage";
+        return "main/mainPage";
     }
 }

3b7c619d276b623e58fd2e767f53441daa35e86e
KangTaeGong <smlee8577@naver.com>
2022-11-07 18:53:55 +0900

    [Create] NaverApiClientInfo.java

---
+++ reviews/src/main/java/project/reviews/api/NaverApiClientInfo.java
@@ -0,0 +1,6 @@
+package project.reviews.api;
+
+public class NaverApiClientInfo {
+    public static final String client_id = "XdxyCwL_eJGrH8UBpluF";
+    public static final String client_secret = "1f8yHJOOWY";
+}

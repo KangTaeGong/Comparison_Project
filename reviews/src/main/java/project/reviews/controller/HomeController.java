7d4e80b7bdbb684e68bad10a4539ce07ea9359f4
KangTaeGong <smlee8577@naver.com>
2022-11-08 19:03:13 +0900

    [Add] HomeController: @PostMapping() - searchItem()

--- reviews/src/main/java/project/reviews/controller/HomeController.java
+++ reviews/src/main/java/project/reviews/controller/HomeController.java
@@ -5,9 +5,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import project.reviews.api.NaverApiClientInfo;
+import project.reviews.api.NaverMovieApi;
 import project.reviews.domain.User;
 import project.reviews.login.SessionConst;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -34,5 +42,50 @@ public class HomeController {
         log.info("Login UserName = {}", loginUser.getUserName());
         model.addAttribute("user", loginUser);
         return "main/mainPage";
+    }
+
+    /*
+    * 검색어 입력에 따라 사용자가 원하는 정보를 가져오는 로직
+    * NaverMovieApi의 search 메소드를 통해서 정보 검색
+    * readBody를 통해서 검색된 정보를 모두 가져온다.
+    * getResultMapping을 통해서 그중 원하는 정보만(fields) Map의 key를 통해서 가져온다.
+    * */
+    @PostMapping("/")
+    public String searchItem(String searchItem1, String searchItem2) {
+
+        log.info("searchItem1 = {}", searchItem1);
+        log.info("searchItem2 = {}", searchItem2);
+
+        try{
+            NaverMovieApi api = new NaverMovieApi();
+            String url = URLEncoder.encode(searchItem1,"UTF-8");
+            String response = api.search(NaverApiClientInfo.client_id, NaverApiClientInfo.client_secret, url);
+
+            // 가져오고 싶은 정보
+            String[] fields = {"title", "image", "pubDate", "director", "actor", "userRating"};
+            Map<String, Object> result = api.getResultMapping(response, fields);
+
+            if(result.size() > 0)
+                log.info("total => " + result.get("total"));
+
+            List<Map<String, Object>> items = (List<Map<String, Object>>) result.get("result");
+
+            for(Map<String, Object> item : items) {
+                log.info("========================================");
+
+                for(String field : fields)
+                    log.info(field + "->" + item.get(field));
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            // encoding error
+            e.printStackTrace();
+        }
+
+        if(searchItem1 != "" && searchItem2 != "") {
+            return "";
+        }
+
+        return "service/servicePage";
     }
 }

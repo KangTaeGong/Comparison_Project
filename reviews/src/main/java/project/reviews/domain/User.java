881838bd3f8b85422045d4cef34d58b2b8137e32
KangTaeGong <smlee8577@naver.com>
2023-02-14 19:03:15 +0900

    [Add] User - Posting, User - Movie : relational mapping

--- reviews/src/main/java/project/reviews/domain/User.java
+++ reviews/src/main/java/project/reviews/domain/User.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 * 2022-09-15 생성
@@ -16,6 +18,7 @@ import javax.persistence.*;
 public class User extends BaseTimeEntity{
 
     @Id @GeneratedValue
+    @Column(name = "USER_PK")
     private Long id;
     private String userName;
     private String userId;
@@ -24,6 +27,12 @@ public class User extends BaseTimeEntity{
     @Enumerated(EnumType.STRING)
     @Setter
     private Role role;
+
+    @OneToMany(mappedBy = "user")
+    List<Movie> movies = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    List<Posting> postings = new ArrayList<>();
 
     public User(String userName, String userId, String password) {
         this.userName = userName;

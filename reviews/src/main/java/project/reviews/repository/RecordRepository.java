32a75549aead50413aa08d896ba88f3a98a006b1
KangTaeGong <smlee8577@naver.com>
2023-02-14 19:03:47 +0900

    [Create] RecordRepository - Spring Data Jpa

---
+++ reviews/src/main/java/project/reviews/repository/RecordRepository.java
@@ -0,0 +1,11 @@
+package project.reviews.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.reviews.domain.Movie;
+
+/*
+* 2023-02-13
+* SpringData JPA 사용
+* */
+public interface RecordRepository extends JpaRepository<Movie, Long> {
+}

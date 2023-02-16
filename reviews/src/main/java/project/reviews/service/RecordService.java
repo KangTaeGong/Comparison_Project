53cb56d352ba9c7d3c9d03c4dc471953888eeac1
KangTaeGong <smlee8577@naver.com>
2023-02-16 19:04:57 +0900

    [Add] RecordService, RecordServiceTest - getMovieList(), getPostingList()

--- reviews/src/main/java/project/reviews/service/RecordService.java
+++ reviews/src/main/java/project/reviews/service/RecordService.java
@@ -4,9 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.reviews.domain.Movie;
+import project.reviews.domain.Posting;
 import project.reviews.domain.User;
+import project.reviews.dto.RecordPostingDto;
 import project.reviews.repository.RecordRepository;
 import project.reviews.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 * 2023-01-12
@@ -20,12 +26,48 @@ public class RecordService {
     private final RecordRepository recordRepository;
     private final UserRepository userRepository;
 
-    public Long save(String movie_title, User user) {
+    /*
+    * 사용자가 검색한 영화 정보를 DB에 저장
+    * User Entity와 연관관계 매핑하여 어느 회원이 영화를 검색했는지 회원 정보를 같이 저장
+    * */
+    public Long saveMovie(String movie_title, User user) {
 
         Movie movie = new Movie(movie_title, userRepository.loadUserByUserId(user.getUserId()));
         recordRepository.save(movie);
 
         return movie.getId();
     }
+    
+    /*
+    * 사용자가 검색한 영화 정보 기록을 가져온다.
+    * 영화의 제목만 화면에 뿌려줄 예정이기 때문에 List에 제목만 담아서 반환
+    * */
+    public List<String> getMovieList(User user) {
 
+        User findUser = userRepository.loadUserByUserId(user.getUserId());
+
+        List<Movie> movies = recordRepository.searchMovieList(findUser.getId(), 0, 5);
+        
+        List<String> list = new ArrayList<>();
+
+        for(Movie movie : movies) {
+            list.add(movie.getMovie_title());
+        }
+        return list;
+    }
+
+    /*
+     * 사용자가 작성한 게시글 정보 기록을 가져온다.
+     * 게시글의 제목과, 게시글의 id만 담아서 리턴
+     * 게시글의 id는 사용자 정보에서 게시글로 href를 통해 바로 이동할 때 사용
+     * */
+    public List<RecordPostingDto> getPostingList(User user) {
+
+        User findUser = userRepository.loadUserByUserId(user.getUserId());
+
+        List<Posting> postings = recordRepository.searchPostingList(findUser.getId(), 0, 5);
+
+        return postings.stream().map(p -> new RecordPostingDto(p.getId(), p.getTitle())).collect(Collectors.toList());
+
+    }
 }
--- reviews/src/test/java/project/reviews/service/RecordServiceTest.java
+++ reviews/src/test/java/project/reviews/service/RecordServiceTest.java
@@ -7,11 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import project.reviews.domain.Movie;
+import project.reviews.domain.Posting;
 import project.reviews.domain.User;
-import project.reviews.login.JoinForm;
+import project.reviews.dto.RecordPostingDto;
+import project.reviews.repository.PostingRepository;
 import project.reviews.repository.RecordRepository;
 import project.reviews.repository.UserRepository;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,11 +27,10 @@ import java.util.Optional;
 @Slf4j
 public class RecordServiceTest {
 
-    @Autowired
-    UserRepository userRepository;
+    @Autowired UserRepository userRepository;
+    @Autowired PostingRepository postingRepository;
     @Autowired RecordService recordService;
-    @Autowired
-    RecordRepository recordRepository;
+    @Autowired RecordRepository recordRepository;
 
     @Test
     void save() {
@@ -66,5 +68,61 @@ public class RecordServiceTest {
         User search_User = search_Movie.getUser();
 
         Assertions.assertEquals("홍길동", search_User.getUserName());
+    }
+
+    @Test
+    void getMovieList_Test() {
+        //given
+        User user1 = new User("홍김동", "aaa1234", "1221@");
+        userRepository.save(user1);
+
+
+        recordService.saveMovie("터미네이터1", user1);
+        recordService.saveMovie("터미네이터2", user1);
+        recordService.saveMovie("터미네이터3", user1);
+
+        //when
+        List<String> movieList = recordService.getMovieList(user1);
+
+        //then
+        for(String movie : movieList) {
+            log.info("movie_title = {}", movie);
+        }
+
+        Assertions.assertEquals(3, movieList.size());
+    }
+
+    @Test
+    void getPostingList_Test() {
+        //given
+        User user1 = new User("김길이", "abc123", "abcd1234@");
+        User user2 = new User("김길삼", "zbc321", "abcd1234@");
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        Posting posting1 = new Posting("테스트 게시글1", "내용입니다.", "김길이", "abcd1233", 1, user1);
+        Posting posting2 = new Posting("테스트 게시글2", "내용입니다.", "김길이", "abcd1233", 1, user1);
+
+        Posting posting3 = new Posting("테스트 게시글3", "내용입니다.", "김길삼", "abcd1233", 1, user2);
+        Posting posting4 = new Posting("테스트 게시글4", "내용입니다.", "김길삼", "abcd1233", 1, user2);
+        Posting posting5 = new Posting("테스트 게시글5", "내용입니다.", "김길삼", "abcd1233", 1, user2);
+
+        postingRepository.create(posting1);
+        postingRepository.create(posting2);
+        postingRepository.create(posting3);
+        postingRepository.create(posting4);
+        postingRepository.create(posting5);
+
+        //when
+        List<RecordPostingDto> postingList = recordService.getPostingList(user2);
+
+        //then
+        for(RecordPostingDto posting : postingList) {
+            log.info("#posting id = {}", posting.getPostingId());
+            log.info("#posting title = {}", posting.getPostingTitle());
+        }
+
+        Assertions.assertEquals(3, postingList.size());
     }
 }
